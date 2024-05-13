b89938d2bd15a6e04c53732f0108fd236d8c26ad
gunnerDgd <ifstudiomai@naver.com>
2024-05-14 01:36:05 +0900

    [Fix] /x86_64/kvm/va.c : VA <-> PA Translation Fixed (VA and VA Length input is required)

--- src/core/x86_64/kvm/va.c
+++ src/core/x86_64/kvm/va.c
@@ -1,5 +1,6 @@
 #include "va.h"
 
+#include "root.h"
 #include "cpu.h"
 #include "pa.h"
 
@@ -22,17 +23,19 @@ bool_t
     vp_va_new
         (vp_va* self, u32_t count, va_list arg)                                 {
             struct vp_cpu *cpu = null_t; if (count > 0) cpu = va_arg(arg, any_t);
-            struct vp_pa  *pa  = null_t; if (count > 1) pa  = va_arg(arg, any_t);
+            reg_t          va  = 0ull  ; if (count > 1) va  = va_arg(arg, reg_t);
+            u64_t          len = 0ull  ; if (count > 2) len = va_arg(arg, u64_t);
 
-            struct kvm_translation va = { .linear_address = pa->pa };
+            struct kvm_translation trans = { .linear_address = va };
             if (trait_of(cpu) != vp_cpu_t) return false_t;
-            if (trait_of(pa)  != vp_pa_t)  return false_t;
-            self->len = pa->len;
+            self->len = len;
             self->cpu = cpu;
-            self->pa  = pa ;
+            self->va  = va ;
 
             if (ioctl(cpu->cpu, KVM_TRANSLATE, &va) < 0) return false_t;
-            self->va = va.physical_address;
+            self->pa = vp_root_pa(cpu->root, trans.physical_address, len);
+
+            if (trait_of(self->pa) != vp_pa_t) return false_t;
             return true_t;
 }
 
