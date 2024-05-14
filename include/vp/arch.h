ddd14701b124ecbd07d8fca72899b3db6de6d6b1
gunnerDgd <ifstudiomai@naver.com>
2024-05-15 04:12:01 +0900

    [Remove] Removed Obsolete Include Headers

--- include/vp/arch.h
+++
@@ -1,6 +0,0 @@
-#ifndef __ARCH_H__
-#define __ARCH_H__
-
-#include "arch/x86_64.h"
-
-#endif
--- include/vp/core/run/halt.h
+++
@@ -1,8 +0,0 @@
-#ifndef __CORE_RUN_HALT_H__
-#define __CORE_RUN_HALT_H__
-
-#include <obj.h>
-
-extern obj_trait* vp_halt_t;
-
-#endif
