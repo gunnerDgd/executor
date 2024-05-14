294b7e32ab65303f64326ae8127f013798dd6229
gunnerDgd <ifstudiomai@naver.com>
2024-05-15 06:39:41 +0900

    [Change] /bus/pio.c : Refactoring on vp_pio_do_XXX

--- src/bus/pio.c
+++ src/bus/pio.c
@@ -8,14 +8,15 @@ bool_t
             if (addr >= 64 KB)             return false_t;
             if (!in)                       return false_t;
 
-            vp_pio_dev *dev = map_find (&pio->pio, (any_t) addr);
-            if (trait_of(dev) == vp_pio_dev_t)
-                return dev->ops->in8         (
-                    dev->dev,
-                    in
-                );
+            node       *nod = map_find (&pio->pio, (any_t) addr);
+            vp_pio_dev *dev = value    (nod);
 
-            return false_t;
+            if (trait_of(dev) != vp_pio_dev_t) return false_t;
+            if (trait_of(nod) != node_t)       return false_t;
+            return dev->ops->in8 (
+                   dev->dev,
+                   in
+            );
 }
 
 bool_t
@@ -25,14 +26,15 @@ bool_t
             if (addr >= 64 KB)             return false_t;
             if (!in)                       return false_t;
 
-            vp_pio_dev *dev = map_find (&pio->pio, (any_t) addr);
-            if (trait_of(dev) == vp_pio_dev_t)
-                return dev->ops->in16        (
-                    dev->dev,
-                    in
-                );
+            node       *nod = map_find (&pio->pio, (any_t) addr);
+            vp_pio_dev *dev = value    (nod);
 
-            return false_t;
+            if (trait_of(dev) != vp_pio_dev_t) return false_t;
+            if (trait_of(nod) != node_t)       return false_t;
+            return dev->ops->in16 (
+                   dev->dev,
+                   in
+            );
 }
 
 bool_t
@@ -42,14 +44,15 @@ bool_t
             if (addr >= 64 KB)             return false_t;
             if (!in)                       return false_t;
 
-            vp_pio_dev *dev = map_find (&pio->pio, (any_t) addr);
-            if (trait_of(dev) == vp_pio_dev_t)
-                return dev->ops->in32        (
-                    dev->dev,
-                    in
-                );
+            node       *nod = map_find (&pio->pio, (any_t) addr);
+            vp_pio_dev *dev = value    (nod);
 
-            return false_t;
+            if (trait_of(dev) != vp_pio_dev_t) return false_t;
+            if (trait_of(nod) != node_t)       return false_t;
+            return dev->ops->in32 (
+                   dev->dev,
+                   in
+            );
 }
 
 bool_t
@@ -65,14 +68,15 @@ bool_t
             if (addr >= 64 KB)             return false_t;
             if (!out)                      return false_t;
 
-            vp_pio_dev *dev = map_find (&pio->pio, (any_t) addr);
-            if (trait_of(dev) == vp_pio_dev_t)
-                return dev->ops->out8        (
-                    dev->dev,
-                    out
-                );
+            node       *nod = map_find (&pio->pio, (any_t) addr);
+            vp_pio_dev *dev = value    (nod);
 
-            return false_t;
+            if (trait_of(dev) != vp_pio_dev_t) return false_t;
+            if (trait_of(nod) != node_t)       return false_t;
+            return dev->ops->out8 (
+                   dev->dev,
+                   out
+            );
 }
 
 bool_t
@@ -82,14 +86,15 @@ bool_t
             if (addr >= 64 KB)             return false_t;
             if (!out)                      return false_t;
 
-            vp_pio_dev *dev = map_find (&pio->pio, (any_t) addr);
-            if (trait_of(dev) == vp_pio_dev_t)
-                return dev->ops->out16       (
-                    dev->dev,
-                    out
-                );
+            node       *nod = map_find (&pio->pio, (any_t) addr);
+            vp_pio_dev *dev = value    (nod);
 
-            return false_t;
+            if (trait_of(dev) != vp_pio_dev_t) return false_t;
+            if (trait_of(nod) != node_t)       return false_t;
+            return dev->ops->out16 (
+                   dev->dev,
+                   out
+            );
 }
 
 bool_t
@@ -99,14 +104,15 @@ bool_t
             if (addr >= 64 KB)             return false_t;
             if (!out)                      return false_t;
 
-            vp_pio_dev *dev = map_find (&pio->pio, (any_t) addr);
-            if (trait_of(dev) == vp_pio_dev_t)
-                return dev->ops->out32       (
-                    dev->dev,
-                    out
-                );
+            node       *nod = map_find (&pio->pio, (any_t) addr);
+            vp_pio_dev *dev = value    (nod);
 
-            return false_t;
+            if (trait_of(dev) != vp_pio_dev_t) return false_t;
+            if (trait_of(nod) != node_t)       return false_t;
+            return dev->ops->out32 (
+                   dev->dev,
+                   out
+            );
 }
 
 bool_t
@@ -142,7 +148,7 @@ bool_t
         (vp_pio* self, u32_t count, va_list arg)                                {
             vp_root *root = null_t; if (count > 0) root = va_arg (arg, vp_root*);
             if (!make_at(&self->pa , vp_pa)  from (3, root, 0, 64 KB))              goto new_err;
-            if (!make_at(&self->bus, vp_bus) from (3, &vp_pio_do, &self->pa, self)) goto new_err;
+            if (!make_at(&self->bus, vp_bus) from (3, &vp_pio_do, self, &self->pa)) goto new_err;
             if (!make_at(&self->pio, map)    from (0))                              goto new_err;
             return true_t;
     new_err:
