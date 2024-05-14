8a30a0224287470a91e8bb1f3aacd2fc307084d1
gunnerDgd <ifstudiomai@naver.com>
2024-05-15 04:17:20 +0900

    [Change] Thread - Safe Implementation - Force vp_XXX modification on the creation thread

--- src/core/x86_64/kvm/root.h
+++ src/core/x86_64/kvm/root.h
@@ -3,6 +3,7 @@
 
 #include <core.h>
 #include <collections.h>
+#include <thread.h>
 
 struct vp_cpu;
 struct vp_pa ;
@@ -11,16 +12,17 @@ extern obj_trait *vp_root_t;
 typedef struct    vp_root  {
     obj   head;
     u64_t root;
+    thd*  thd;
     u64_t map;
     map   cpu,
           pa ;
 }   vp_root;
 
-bool_t         vp_root_new    (vp_root*, u32_t, va_list);
-bool_t         vp_root_clone  (vp_root*, vp_root*)      ;
-void           vp_root_del    (vp_root*)                ;
+bool_t         vp_root_new  (vp_root*, u32_t, va_list);
+bool_t         vp_root_clone(vp_root*, vp_root*)      ;
+void           vp_root_del  (vp_root*)                ;
 
-struct vp_cpu* vp_root_cpu    (vp_root*, u64_t)       ;
-struct vp_pa*  vp_root_pa     (vp_root*, reg_t, u64_t);
+struct vp_cpu* vp_root_cpu  (vp_root*, u64_t)       ;
+struct vp_pa*  vp_root_pa   (vp_root*, reg_t, u64_t);
 
 #endif
