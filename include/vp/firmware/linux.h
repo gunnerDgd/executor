d0ad6740051491819610f3b3bb925ec32cb7dbc8
gunnerDgd <ifstudiomai@naver.com>
2024-05-15 20:34:43 +0900

    [Feature] Linux Boot Protocol / kernel, initrd Loader Adding (/firmware/linux.c)

---
+++ include/vp/firmware/linux.h
@@ -0,0 +1,9 @@
+#ifndef VAPOR_FIRMWARE_LINUX_H
+#define VAPOR_FIRMWARE_LINUX_H
+
+#include <core.h>
+
+extern obj_trait *linux_firm_t;
+typedef struct    linux_firm { u8_t firm[128]; } linux_firm;
+
+#endif
