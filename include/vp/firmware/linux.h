f061e36fc9ad8d62ba35a41c028f8193485c64d4
gunnerDgd <ifstudiomai@naver.com>
2024-05-21 03:11:45 +0900

    [Remove] Removed Old Linux Boot Injection Layer

--- include/vp/firmware/linux.h
+++
@@ -1,9 +0,0 @@
-#ifndef VAPOR_FIRMWARE_LINUX_H
-#define VAPOR_FIRMWARE_LINUX_H
-
-#include <core.h>
-
-extern obj_trait *linux_firm_t;
-typedef struct    linux_firm { u8_t firm[128]; } linux_firm;
-
-#endif
